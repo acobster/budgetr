4aec4c9690520365db7c9bce8038ef43d2d32130
The Ambassador of Funk <acobster@gmail.com>
2014-05-07 18:38:37 -0700

    MySQL credentials

--- DB.php
+++ DB.php
@@ -1,28 +1,28 @@
 <?php
 
 class DB {
-    
+
     private static $singleton;
     private $connection;
-    
+
     static function singleton() {
         if( empty( self::$singleton ) ) {
             self::$singleton = new DB();
         }
-        
+
         return self::$singleton;
     }
-    
+
     protected function __construct() {
 
         $this->connection = new PDO(
-            'mysql:dbname=budgetr;host=localhost',
-            'budget',
+            'mysql:dbname=fangchia_budgetr;host=localhost',
+            'fangchia_budgetr',
             'uL2YcVJPseU5XBBC');
     }
-    
+
     function run( $sql ) {
-        
+
         $result = $this->connection->query( $sql );
 
         if($result === false) {
@@ -34,15 +34,15 @@ class DB {
 //echo '<pre>'.var_export($result,true).'</pre>';
         return $result;
     }
-    
+
     function runSingle( $sql ) {
-        
+
         $result = $this->run( $sql );
-        
+
         if( count($result) > 1 ) {
             echo 'found multiple rows in ' . __FUNCTION__;
         }
-        
+
         return $result[0];
     }
 }
