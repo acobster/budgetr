279725efcc53423eeafb58df1c0fc3efe0018f4a
Coby Tamayo <me@cobytamayo.com>
2021-04-12 02:42:44 -0400

    fix styles

--- templates/budget.php
+++ templates/budget.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 
-<html>
+<html lang="en">
 
 <head>
 <meta charset="utf-8" />
 <title>Budgetr</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="css/style.css" />
 <link rel="stylesheet" type="text/css" href="css/tablesorter/style.css" />
 <script type="text/javascript" src="js/jquery.min.js"></script>
